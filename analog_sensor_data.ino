a2e2eb74e45310b05fccfa56f9c7390240feb58c
bradzieminski <bziemins@usc.edu>
2018-04-27 16:55:49 -0700

    sensor data

---
+++ analog_sensor_data.ino
@@ -0,0 +1,31 @@
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin1 = A0;  // Analog input pin that the potentiometer is attached to
+const int analogInPin2 = A1;  // Analog input pin that the potentiometer is attached to
+const int analogInPin3 = A2;  // Analog input pin that the potentiometer is attached to
+const int analogInPin4 = A3;  // Analog input pin that the potentiometer is attached to
+
+void setup() 
+{
+  
+  Serial.begin(19200);
+
+}
+
+void loop() 
+{
+ 
+  int temp1 = analogRead(analogInPin1);
+  int temp2 = analogRead(analogInPin2);
+  int temp3 = analogRead(analogInPin3);
+  int temp4 = analogRead(analogInPin4);
+
+  Serial.println (temp2);
+  delay(10);
+  /*Serial.println (temp1);
+  delay(10);
+  Serial.println (temp1);
+  delay(10);
+  Serial.println (temp1);
+  delay(10);*/
+
+}
