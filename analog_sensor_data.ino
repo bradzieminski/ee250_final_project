9287a46d09d86b282748d70a7580371ce4f353cf
bradzieminski <bziemins@usc.edu>
2018-04-27 21:00:03 -0700

    Add files via upload

--- analog_sensor_data.ino
+++ analog_sensor_data.ino
@@ -1,31 +1,52 @@
+
+#include <math.h>
+
 // These constants won't change. They're used to give names to the pins used:
-const int analogInPin1 = A0;  // Analog input pin that the potentiometer is attached to
-const int analogInPin2 = A1;  // Analog input pin that the potentiometer is attached to
-const int analogInPin3 = A2;  // Analog input pin that the potentiometer is attached to
-const int analogInPin4 = A3;  // Analog input pin that the potentiometer is attached to
+// Analog input pin that the potentiometer is attached to
+int pins[] = { A0, A1, A2, A3, };
+
+#define S1 0
+#define S2 1
+#define S3 2
+#define S4 3
+
+#define wSize 15
+#define maxD 99
+
+int windows[4][wSize] = { {}, {}, {}, {} };
+int avg[wSize] = { };
+int index = 0;
 
 void setup() 
-{
-  
+{  
   Serial.begin(19200);
-
 }
 
 void loop() 
 {
- 
-  int temp1 = analogRead(analogInPin1);
-  int temp2 = analogRead(analogInPin2);
-  int temp3 = analogRead(analogInPin3);
-  int temp4 = analogRead(analogInPin4);
-
-  Serial.println (temp2);
-  delay(10);
-  /*Serial.println (temp1);
-  delay(10);
-  Serial.println (temp1);
-  delay(10);
-  Serial.println (temp1);
-  delay(10);*/
-
+  if (index == wSize)
+  {
+    index = 0;
+  }
+  for (int s = 0; s < 4; s++)
+  {
+    windows[s][index] = fmin(maxD, analogRead(pins[s]));
+    avg[s] = 0;
+    for (int n = 0; n < wSize; n++)
+    {
+      avg[s] += windows[s][n];
+    }
+    avg[s] /= wSize;
+    avg[s] /= 2;
+    avg[s] *= 2;
+  }
+  index++;
+  
+  Serial.println(avg[S1]); 
+  /*Serial.print("   ");
+  Serial.print(avg[S2]); 
+  Serial.print("   ");
+  Serial.print(avg[S3]); 
+  Serial.print("   ");
+  Serial.println(avg[S4]);*/
 }
